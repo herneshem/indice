ba28e409904049aabe1c99b64dee0fc25c326242
herneshem <ernestoami@hotmail.com>
2021-10-07 01:49:16 +0200

    nuevo deploy

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const App = () => {
     <div className="card mb-3 p-1 mb-2 " >
                 <div className="row g-0">
                     <div className="col-md-4">
-                     <img src={imagen.img5} className="img-fluid rounded-start" alt="..."></img>
+                     <img src={imagen.img2} className="img-fluid rounded-start" alt="..."></img>
                     </div>
                     <div className="col-md-8">
                         <div className="card-body text-center">
@@ -29,9 +29,17 @@ const App = () => {
                     </div>
                 </div>
             </div>
-            
-      
-            
+            <Ofertas></Ofertas>
+      <Router>
+            <Switch>
+           
+        
+
+        
+
+        </Switch>
+    </Router>
+
     </Fragment>
   )
 
